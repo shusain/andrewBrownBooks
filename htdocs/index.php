0f4b2920352a2b658a6ca4727f51771d58bbe93f
Kneight Reinagel <Kneight.Reinagel@gmail.com>
2013-08-14 13:52:50 -0500

    August 14th. More readable code and made it an SPA

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,13 +3,16 @@
 <head>
 	<title>Pyramid Players Productions</title>
 	<link rel="stylesheet" type="text/css" href="testing/css/master.css">
+	<script scr="components/mainApp.js"></script>
+	<script>
+	</script>
 </head>
 <body>
 	<div id="outerContainer">
 		<div style="height:20px"></div>
 		<div id="contentRegion">
 			<article class="contentContainer">
-				<img src="testing/images/header.png" alt="Welcome Image" id="headerLogo">
+				<img src="testing/images/header.png" alt="Welcome Image" id="headerLogo" />
 				<h1>
 					Coming soon...
 				</h1>
